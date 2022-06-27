19cf53c4cf5e49402a0e17e606d2e0681f4c39a0
Rahul Gimhavanekar <rahulgimhavanekar@gmail.com>
2022-06-27 09:29:15 +0530

    fix: Changed class to className in Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -40,47 +40,47 @@ const Footer = () => {
       </div>
       <div className={classes.middle}>
         <div className={micClasses} onClick={micClickHandler}>
-          <span class={`material-icons ${classes.md_light} `}>
+          <span className={`material-icons ${classes.md_light} `}>
             {streamState.mic ? "mic" : "mic_off"}
           </span>
         </div>
         <div className={videoClasses} onClick={videoClickHandler}>
-          <span class={`material-icons-outlined ${classes.md_light}`}>
+          <span className={`material-icons-outlined ${classes.md_light}`}>
             {streamState.video ? "videocam" : "videocam_off"}
           </span>
         </div>
         <div className={classes.mid_icon}>
-          <span class={`material-icons-outlined ${classes.md_light}`}>
+          <span className={`material-icons-outlined ${classes.md_light}`}>
             closed_caption_off
           </span>
         </div>
         <div className={classes.mid_icon}>
-          <span class={`material-icons-outlined ${classes.md_light}`}>
+          <span className={`material-icons-outlined ${classes.md_light}`}>
             present_to_all
           </span>
         </div>
         <div className={classes.mid_icon}>
-          <span class={`material-icons-outlined ${classes.md_light}`}>
+          <span className={`material-icons-outlined ${classes.md_light}`}>
             more_vert
           </span>
         </div>
         <div className={`${classes.mid_icon} ${classes.phone}`}>
-          <span class={`material-icons ${classes.md_light}`}>call_end</span>
+          <span className={`material-icons ${classes.md_light}`}>call_end</span>
         </div>
       </div>
       <div className={classes.right}>
         <div className={classes.right_icon}>
-          <span class={`material-icons-outlined ${classes.md_light}`}>
+          <span className={`material-icons-outlined ${classes.md_light}`}>
             info
           </span>
         </div>
         <div className={classes.right_icon}>
-          <span class={`material-icons-outlined ${classes.md_light}`}>
+          <span className={`material-icons-outlined ${classes.md_light}`}>
             people
           </span>
         </div>
         <div className={classes.right_icon}>
-          <span class={`material-icons-outlined ${classes.md_light}`}>
+          <span className={`material-icons-outlined ${classes.md_light}`}>
             chat
           </span>
         </div>
